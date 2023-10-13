65fbb4e7dafb86e34718721b35b4c27700e77f69
PIERO ZAMORA <pierozamora54@gmail.com>
2023-10-13 15:57:14 -0500

    Primer Commit

---
+++ src/main/java/com/upao/clothcycle/Cloth/_Cycle/_API/ClothCycleApiApplication.java
@@ -0,0 +1,13 @@
+package com.upao.clothcycle.Cloth._Cycle._API;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClothCycleApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClothCycleApiApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/upao/clothcycle/Cloth/_Cycle/_API/login/domain/User.java
@@ -0,0 +1,14 @@
+package com.upao.clothcycle.Cloth._Cycle._API.login.domain;
+
+public class User {
+    public String username;
+
+    xcfbvdfbfgdfgdfg
+    fgfsdgsfdgdfgdfg
+            gfdgdfgd
+    fgdfgdfgdfgdfgdfg
+                    dfgdfgdfg
+
+    dfgdfgdfgsfgdf
+
+}
